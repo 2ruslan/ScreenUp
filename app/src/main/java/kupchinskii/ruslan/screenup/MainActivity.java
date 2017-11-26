c248300e2505678c8ee47e8208b52ffb2209dc12
ruslan kupchinskii <2ruslank@gmail.com>
2017-11-26 19:24:35 +0200

    1.0

--- app/src/main/java/kupchinskii/ruslan/screenup/MainActivity.java
+++ app/src/main/java/kupchinskii/ruslan/screenup/MainActivity.java
@@ -1,12 +1,13 @@
 package kupchinskii.ruslan.screenup;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     CheckBox cbStart;
 
